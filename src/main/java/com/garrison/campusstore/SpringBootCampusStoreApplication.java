e1607c51a5a903676e8c9c6e032b4458f16408e6
GarrisonGE <605265195@qq.com>
2021-01-14 15:24:50 -0800

    Implement the SpringBoot project for Campus Store

---
+++ src/main/java/com/garrison/campusstore/SpringBootCampusStoreApplication.java
@@ -0,0 +1,13 @@
+package com.garrison.campusstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCampusStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCampusStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garrison/campusstore/SpringBootCampusStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garrison.campusstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCampusStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
